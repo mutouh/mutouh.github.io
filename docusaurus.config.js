8b6883b62eae2deea935e70d9acdd1f3aebe4ad8
mutouh <mutouh@qq.com>
2023-02-21 22:25:00 +0800

    modify deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mutouh', // Usually your GitHub org/user name.
   projectName: 'mutouh.github.io', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
